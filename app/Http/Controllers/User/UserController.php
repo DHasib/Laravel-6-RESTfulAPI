d0844724ce28f715179652e7b7f0f871bcc5ebb0
DHasib <mdhasib522@gmail.com>
2020-01-07 21:05:30 +0600

    user api CRUE completed

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\User;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Validator;
+
 
 class UserController extends Controller
 {
@@ -15,10 +17,11 @@ class UserController extends Controller
      */
     public function index()
     {
+        //fetch all the user data............................
         $users = User::all();
-
+       //return fetch data into json format......................
         return response()->json(['data' => $users], 200);
-        //return $users;
+       
     }
 
     /**
@@ -29,7 +32,23 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       //validate user data for request to store/create..............
+       $rules = Validator::make($request->all(),[
+               'name'     =>  "required",
+               'email'    =>  " required|email|unique:users",
+               'password' =>  "required|min:6|confirmed",
+       ])->validate();
+
+       $data = $request->all();
+       $data['password'] = bcrypt($request->password);
+       $data['verified'] = User::UNVERIFIED_USER;
+       $data['verification_token'] = User::generateVerificationCode();
+       $data['admin'] = User::REGULAR_USER;
+
+       $user = User::create($data);
+
+       return response()->json(['data' => $user], 201);
+
     }
 
     /**
@@ -55,7 +74,48 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+
+    //fetch the user data using auth user id............................
+        $user = User::findOrFail($id);
+    //validate user data for request to Update..............
+        $rules = Validator::make($request->all(),[
+            'email'     =>  "email|unique:users,email," . $user->id,
+            'password'  =>  "min:6|confirmed",
+            'admin'     =>  "in:" . User::ADMIN_USER . "," . User::REGULAR_USER,
+        ])->validate();
+
+    //check user name field is filled and then insert name into database..............
+      if($request->has('name')){
+          $user->name = $request->name;
+      }
+    //check user email field is filled and also check email is unique or not then insert email into database..............
+      if($request->has('email') && $user->email != $request->email){
+         $user->verified = User::UNVERIFIED_USER;
+         $user->verification_token = User::generateVerificationCode();
+         $user->email = $request->email;
+      }
+    //check password field is filled and then hash password and insert password into database..............
+      if($request->has('password')){
+        $user->password = bcrypt($request->password);
+      }
+
+      //first if condition check user request has admin or not........................
+       if ($request->has('admin')) {
+           //2nd if condition check admin request user verified or not................................
+           if (!$user->isVerified()) {
+              return response()->json(['error' => 'Only verified user can modify the admin field', 'code' => 409], 409);
+           }
+         $user->admin = $request->admin;
+       }
+
+       //This if condition check user Updated value into database or not and through an message..............................
+       if (!$user->isDirty()) {
+            return response()->json(['error' => 'You need to specify a different Value to Update', 'code' => 422], 422);
+       }
+         
+       $user->save();
+
+       return response()->json(['data' => $user], 200);
     }
 
     /**
