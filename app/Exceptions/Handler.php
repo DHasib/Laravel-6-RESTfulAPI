92215f8d5630faf9b758bf06ba7f4c991eb86d17
DHasib <mdhasib522@gmail.com>
2020-01-31 01:38:22 +0600

    Handling Authentication and Authorization Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,9 @@ namespace App\Exceptions;
 
 use Exception;
 use App\Traits\ApiResponser;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
@@ -62,10 +64,31 @@ class Handler extends ExceptionHandler
             return $this->errorResponse("Does not exists any {$modelName} with the specified identificator", 404);
         }
 
+        if ($exception instanceof AuthenticationException) {
+            
+            return $this->unauthenticated($request, $exception);
+         }
+
+         if ($exception instanceof AuthorizationException) {
+            
+            return $this->errorResponse($exception->getMessage(), 403);
+         }
+         
         return parent::render($request, $exception);
     }
 
 
+     /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('Unauthenticated.', 401);
+    }
 
       /**
      * Create a response object from the given validation exception.
